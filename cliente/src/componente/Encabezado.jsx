d13a387e5cc4f478ceac7568669f7252ef17fc64
Tadeo Ramirez <tadeo.ramirez.950208@gmail.com>
2024-03-25 10:08:45 -0400

    nuevo commit actualizacion

--- cliente/src/componente/Encabezado.jsx
+++ cliente/src/componente/Encabezado.jsx
@@ -6,9 +6,7 @@ const Encabezado = () => {
   return (
     <div className='container-fluid p-0 cabezeraPrincipal '>
       <header className="cabecera_nuevo">       
-        <div className="cabezera_img container">
-          <img className="imgLogo mt-3" src="/AQUI URL PARA UN LOGO" alt="" />
-        </div>
+
         <div className="divisoria">
           <h1 className="text-center Titulo">COOPERATIVA</h1>
           <h4 className=''> Somos una entidad de reconocida solvencia que brinda servicios Financieros y Sociales.</h4>
